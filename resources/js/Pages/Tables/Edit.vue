c068a0289a627abc85b8ce12518e659787a68446
Tekz00r <elderscry@gmail.com>
2023-06-02 16:27:52 +0200

    fixed input edit

--- resources/js/Pages/Tables/Edit.vue
+++ resources/js/Pages/Tables/Edit.vue
@@ -1,10 +1,17 @@
 <template>
     <app-layout>
-        <div>
-            enter title
-            <input v-model="form.name" type="text" class="shadow appearance-none border rounded w-[80%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="Enter Title">
+        <div class="inline-block flex">
+            <p class="my-auto mx-auto">Enter title</p>
+            <input v-model="form.name" type="text" class="shadow appearance-none border rounded w-[80%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ml-auto flex " id="exampleFormControlInput1" placeholder="Enter Title">
         </div>
-        <input v-model="form.name" type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="Enter Title">
+        <div class="inline-block flex">
+            <p class="my-auto mx-auto">Enter title</p>
+            <input v-model="form.name" type="text" class="shadow appearance-none border rounded w-[80%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ml-auto flex " id="exampleFormControlInput1" placeholder="Enter Title">
+        </div>
+        <div class="inline-block flex">
+            <p class="my-auto mx-auto">Enter title</p>
+            <input v-model="form.name" type="text" class="shadow appearance-none border rounded w-[80%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ml-auto flex " id="exampleFormControlInput1" placeholder="Enter Title">
+        </div>
     </app-layout>
 </template>
 
